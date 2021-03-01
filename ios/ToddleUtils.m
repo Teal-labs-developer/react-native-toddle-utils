72cd5b0040fe27098984d7b40014556999a16fee
dhrumilpatel <dhrumil@toddleapp.com>
2021-03-01 17:45:07 +0530

    MAJOR: isCallConnected function added in iOS

--- ios/ToddleUtils.m
+++ ios/ToddleUtils.m
@@ -11,4 +11,21 @@ RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnu
 	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
 }
 
+RCT_EXPORT_METHOD(isCallConnected:(RCTResponseSenderBlock)callback)
+{
+    NSString* result = @"disconnected";
+    if (@available(iOS 10.0, *)) {
+        CXCallObserver *callObserver = [[CXCallObserver alloc] init];
+        NSArray<CXCall *> *calls = [callObserver calls];
+        // iterate over calls
+        for (CXCall *call in calls) {            
+            if(![call hasEnded]){
+                result = @"connected";
+            }
+        }
+    }
+    
+    callback(@[result]);
+}
+
 @end
